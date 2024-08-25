8a12eda07fef5078fea322eadb08d9de19766d49
misiro2000 <aoi061529@gmail.com>
2024-08-25 09:25:52 +0900

    タイトルと項目名を修正

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ data_file = 'taxi_data.csv'
 
 # 初期化: データファイルがなければヘッダーを作成
 if not os.path.isfile(data_file):
-    df = pd.DataFrame(columns=['日付', '住所', '時間', '金額', '支払い形態'])
+    df = pd.DataFrame(columns=['日付', '23区', '時間', '金額', '支払い形態'])
     df.to_csv(data_file, index=False)
 
 # 支払い形態と東京23区のリスト
-payment_methods = ["現金", "クレジットカード", "電子マネー", "QRコード決済"]
+payment_methods = ["現金", "クレジットカード", "電子マネー", "QRコード決済","GoPAY"]
 tokyo_districts = ["千代田区", "中央区", "港区", "新宿区", "文京区", ...]
 
-st.title('タクシー管理システム')
+st.title('営業統計管理システム')
 
 # ページの選択
 page = st.sidebar.selectbox('ページを選択', ['データ入力', '統計情報'])
 
 if page == 'データ入力':
-    st.header('タクシー情報の入力')
+    st.header('営業情報の入力')
     date = st.date_input('日付')
-    address = st.selectbox('住所', tokyo_districts)
+    address = st.selectbox('23区', tokyo_districts)
     time_input = st.text_input('時間 (HH:MM)')
     amount = st.number_input('金額', min_value=0, format='%d')
     payment_method = st.selectbox('支払い形態', payment_methods)
@@ -38,7 +38,7 @@ if page == 'データ入力':
 
     if st.button('送信') and valid_time:
         new_data = pd.DataFrame([[date, address, time.strftime("%H:%M"), amount, payment_method]],
-                                columns=['日付', '住所', '時間', '金額', '支払い形態'])
+                                columns=['日付', '23区', '時間', '金額', '支払い形態'])
         new_data.to_csv(data_file, mode='a', header=False, index=False)
         st.success('記録が正常に追加されました！')
 
@@ -54,18 +54,18 @@ elif page == '統計情報':
     if option == '日間':
         selected_date = st.date_input("日付を選択")
         filtered_data = df[df['日付'] == pd.Timestamp(selected_date)]
-        summary = filtered_data.groupby('住所')['金額'].sum()
+        summary = filtered_data.groupby('23区')['金額'].sum()
     elif option == '月間':
         year_month = st.selectbox('年月を選択', pd.unique(df['年'].astype(str) + '-' + df['月'].astype(str)))
         year, month = map(int, year_month.split('-'))
         filtered_data = df[(df['年'] == year) & (df['月'] == month)]
-        summary = filtered_data.groupby('住所')['金額'].sum()
+        summary = filtered_data.groupby('23区')['金額'].sum()
     elif option == '年間':
         selected_year = st.selectbox('年を選択', pd.unique(df['年']))
         filtered_data = df[df['年'] == selected_year]
-        summary = filtered_data.groupby('住所')['金額'].sum()
+        summary = filtered_data.groupby('23区')['金額'].sum()
     elif option == '累計':
-        summary = df.groupby('住所')['金額'].sum()
+        summary = df.groupby('23区')['金額'].sum()
 
     st.subheader(f'{option}の集計')
     st.bar_chart(summary)
